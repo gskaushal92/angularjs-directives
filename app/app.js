f39f2042fa47c3bf910687be49f2b9494c269726
Gopal Sharma <P7111387@NESSINBLR.COM>
2017-10-12 17:07:44 +0530

    input drpdwn directive

--- app/app.js
+++ app/app.js
@@ -1,45 +1,50 @@
 angular.module('inputDropdown',['ngRoute'])
 
 .config(function(  $locationProvider,$routeProvider) {
-  $locationProvider.hashPrefix('!')
-  $routeProvider.otherwise({
-    templateUrl : "index.html"})
+    $locationProvider.hashPrefix('!')
+    $routeProvider.otherwise({
+      templateUrl : "index.html"})
   })
 .controller("MainCtrl",['$scope',function($scope){
-  $scope.countries=["Select","India","UK","US"]
-    //
-    //
-    // let ele=angular.element('#country')
-    // ele.on('mouseenter',function(){
-    //   $scope.countryList=true;
-    // })
-    // ele.on('mouseout',function(){
-    //   $scope.countryList=false;
-    // })
-}])
-.directive('inputDrpDwn',function(){
+    $scope.countries=["Select","India","UK","US"]
 
-})
+    $scope.addCountry=function(){
+      if($scope.countries.indexOf($scope.country)===-1){
+        $scope.countries.push($scope.country)
+      }
+    }
+
+    $scope.countryChange =function(){
+      if($scope.country && $scope.country !=='Select'){
+        $scope.btnDisable=false;
+      }
+      else {
+        $scope.btnDisable=true;
+      }
+    }
+}])
 .directive('showHideList', function () {
     return {
       scope:{
         countries:'=',
-        country:'=',
-        listShow:'='
+        country:'='
       },
-      template:'<input type="text" id="country" name="conutries" ng-model="country">'+
-                '<ul ng-repeat="cntry in countries" ng-show="listShow">'+
-                  '<li class="country">{{cntry}}</li>'+
-                '</ul>',
-        link: function (scope, element, attrs) {
-            element.on('mouseenter', function () {
-                //scope.listShow=true
-                scope.$apply('listShow = true');
-            });
-            element.on('mouseout', function () {
-                //scope.listShow=false
-                scope.$apply('listShow = false');
-            });
+      template:'<div  ng-mouseenter="mouseeneter()" ng-mouseleave="mouseleave()"><input type="text" id="country" name="country" ng-model="country">'+
+                '<div ng-show="listShow"><ul ng-repeat="cntry in countries | filter:country">'+
+                  '<li class="country" ng-click="select(cntry)">{{cntry}}</li>'+
+                '</ul></div></div>',
+      controller:function ($scope, $element) {
+        $scope.listShow= false;
+        $scope.mouseeneter =function(){
+          $scope.listShow=true;
         }
+        $scope.mouseleave=function(){
+          $scope.listShow=false;
+        }
+        $scope.select=function(cntry){
+          $scope.country=cntry
+          $scope.listShow=false;
+        }
+      }
     };
 });
